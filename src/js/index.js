dff4e1a6cc97fcbf23101b073f8a46e8b47938a6
Vadim <svanovivan@gmail.com.com>
2020-04-27 11:50:44 +0300

    Rewrite hw-12

--- src/js/index.js
+++ src/js/index.js
@@ -1,54 +1,65 @@
 import '../styles/styles.css';
 import _ from 'lodash';
-import galleryTemplate from '../templates/gallery-templates.hbs';
-import apiService from './services/apiService.js';
+import countriesTemplate from '../templates/countries-card-template.hbs';
+import countryService from './services/search_country_service.js';
 import '@pnotify/core/dist/BrightTheme.css';
 import { alert, error, defaults, Stack } from '@pnotify/core/dist/PNotify';
 import '@pnotify/core/dist/PNotify.css';
 import 'material-design-icons/iconfont/material-icons.css';
 
 const refs = {
-  re: document.querySelector('body'),
-  searchForm: document.querySelector('#search-form'),
-  gallery: document.querySelector('#gallery'),
-  loadMoreBtn: document.querySelector(`button[data-action="load-more"]`),
+  searchForm: document.querySelector('#search_form'),
+  card: document.querySelector('.js-card'),
 };
 
-refs.searchForm.addEventListener('submit', searchFormSubmitHandler);
-refs.loadMoreBtn.addEventListener('click', loadMoreBtnHandler);
+refs.searchForm.addEventListener(
+  'input',
+  _.debounce(searchFormSubmitHandler, 1500),
+);
 
 function searchFormSubmitHandler(e) {
   e.preventDefault();
-  const searchQuery = e.currentTarget.query.value;
-  apiService.resetPage();
-  apiService.searchQuery = searchQuery;
-
-  e.currentTarget.query.value = '';
   clearMarkup();
 
-  apiService.fetchImages().then(buildListItemsMarkup);
+  const inputValue = e.target.value;
+  countryService.searchQuery = inputValue;
+  e.target.value = '';
+
+  countryService
+    .fetchCountry()
+    .then(data => {
+      if (data.length < 2) {
+        buildCardCountry(data);
+      } else if ((data.length > 2) & (data.length < 10)) {
+        buildCardCountryList(data);
+      } else if (data.length > 10) {
+        alert({
+          text: 'Too many matches found. Please enter a more specific query!',
+          maxTextHeight: null,
+          width: '400px;',
+        });
+      } else
+        error({
+          text: 'No such country found.',
+          maxTextHeight: null,
+          width: '400px;',
+        });
+    })
+    .catch(error => console.log(error));
 }
 
-function loadMoreBtnHandler() {
-  apiService.fetchImages().then(buildListItemsMarkup);
+function buildCardCountry(item) {
+  const markup = countriesTemplate(item);
+
+  refs.card.insertAdjacentHTML('beforeend', markup);
 }
 
-function buildListItemsMarkup(items) {
-  const position = document.body.offsetHeight;
-  console.log(position);
-
-  const markup = galleryTemplate(items);
-  refs.gallery.insertAdjacentHTML('beforeend', markup);
-  Scroll(position);
+function buildCardCountryList(items) {
+  const markup = items.map(item =>
+    refs.card.insertAdjacentHTML('beforeend', `<li>${item.name}</li>`),
+  );
 }
 
 function clearMarkup() {
-  refs.gallery.innerHTML = '';
-}
-
-function Scroll(position) {
-  window.scrollTo({
-    top: position,
-    behavior: 'smooth',
-  });
+  refs.card.innerHTML = ' ';
 }
